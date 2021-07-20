169ac880be551314e893dd2fcee9799f29cc0d28
Luca Simi <lucasimi90@gmail.com>
2021-07-20 21:58:29 +0200

    Added ballTree algorithm

--- src/Data/Tree/BallTree.hs
+++ src/Data/Tree/BallTree.hs
@@ -2,20 +2,62 @@ module Data.Tree.BallTree where
 
 import Control.Monad.ST
 import qualified Data.Vector as V
+import qualified Data.Set as S
 import qualified Data.Vector.Mutable as VM
+import System.Random
+import Data.Vector.Sort
 
-data BinTree a = BinTree
-    { value :: a
-    , left  :: BinTree a
-    , right :: BinTree a }
+data BinTree a b 
+    = Empty
+    | Leaf b
+    | Node    
+        { value :: a
+        , left  :: BinTree a b
+        , right :: BinTree a b }
 
 data Ball a b = Ball
     { center :: a
     , radius :: b }
 
-data BallTree a b = BallTree
-    { metric :: a -> a -> b 
-    , tree   :: BinTree (Ball a b) }
+ball :: Ord b => (a -> a -> b) -> Ball a b -> V.Vector a -> V.Vector a
+ball m b = V.filter $ \x -> m (center b) x < radius b
 
-ballTreeST :: VM.MVector s a -> (a -> a -> b) -> ST s ()
-ballTreeST vec m = undefined
+type BallTree a b = BinTree (Ball a b) a
+
+ballTreeST :: (RandomGen g, Ord b) => g -> VM.MVector s a -> (a -> a -> b) -> ST s (BallTree a b)
+ballTreeST g vec m
+    | VM.length vec == 0 = return Empty
+    | VM.length vec < 2  = do
+        p <- VM.read vec 0
+        return $ Leaf p
+    | otherwise          = do
+        let (pivot, g') = uniformR (0, VM.length vec - 1) g
+            mid = VM.length vec `div` 2
+        center <- VM.read vec pivot
+        sortAtST vec mid (m center)
+        furth <- VM.read vec mid
+        lft <- ballTreeST g' (VM.slice 0 mid vec) m
+        rgt <- ballTreeST g' (VM.slice mid (VM.length vec - mid) vec) m 
+        return $ Node 
+            { value = Ball 
+                { center = center
+                , radius = m center furth }
+            , left  = lft
+            , right = rgt }
+
+ballTree :: Ord b => V.Vector a -> (a -> a -> b) -> BallTree a b
+ballTree vec m = runST $ do
+    vec' <- V.thaw vec
+    ballTreeST (mkStdGen 42) vec' m
+
+ballSearch :: (Ord a, RealFloat b) => BallTree a b -> (a -> a -> b) -> Ball a b -> S.Set a
+ballSearch Empty _ _                                = S.empty  
+ballSearch (Leaf c) m (Ball p eps)                  = if m p c < eps then S.singleton c else S.empty
+ballSearch (Node (Ball c r) lft rgt) m (Ball p eps) = S.union lftSearch rgtSearch
+  where
+    lftSearch = if m p c > r + eps 
+        then S.empty 
+        else ballSearch lft m (Ball p eps)
+    rgtSearch = if m p c <= r - eps 
+        then S.empty 
+        else ballSearch rgt m (Ball p eps)
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,13 +2,18 @@ import Test.QuickCheck
 import Test.QuickCheck.Instances.Vector
 
 import qualified Data.Vector as V
+import qualified Data.Set as S
 import Data.Vector.Sort
+import Data.Tree.BallTree
 
 main :: IO ()
 main = do
     putStrLn "Testing sortAt"
     quickCheck sortAtOrder
+    putStrLn "Testing quicksort"
     quickCheck quicksortOrder
+    putStrLn "Testing ballTree"
+    quickCheck ballTreeRetrieval
 
 sortAtOrder :: V.Vector Float -> Bool 
 sortAtOrder vec = and [isSorted (sort i) i j | i <- ids, j <- ids]
@@ -29,3 +34,11 @@ quicksortOrder vec = and [isSorted (qs vec) i j | i <- ids, j <- ids]
     isSorted vec' i j
         | i <= j    = vec' V.! i <= vec' V.! j
         | otherwise = vec' V.! i >= vec' V.! j
+
+ballTreeRetrieval :: V.Vector Float -> Float -> Bool
+ballTreeRetrieval vec r = and [search vec m (x + s/2) s == ballSearch t m (Ball (x + s/2) s) | x <- V.toList vec]
+  where
+    s = 0.0001 + abs r
+    m = \x y -> abs $ x - y
+    t = ballTree vec m
+    search vec' m' x' r' = S.fromList $ V.toList $ V.filter (\y -> m' x' y <= r') vec'
